d492f2896c55e61496a7d61b364c5be8f20c0691
zvpaman <144277432+zvpaman@users.noreply.github.com>
2025-01-28 14:51:19 -0500

    no blanks

---
+++ exercises/ex01_tea_party.py
@@ -0,0 +1,9 @@
+"""This program calculates the stuff needed for a
+tea party based on the number of guests."""
+
+__author__: str = "730466088"
+
+
+def tea_bags(people: int) -> int:
+    """Calculate the number of tea bags needed based on the number of guests."""
+    return people * 2
